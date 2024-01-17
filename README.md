f38c01fea821bf71f3e072fb44d25bd11518d057
ojpro <contact@ojpro.me>
2024-01-17 19:33:04 +0100

    Initial project setup ✨

---
+++ README.md
@@ -0,0 +1,3 @@
+# News App
+
+Create a simple news app using flutter and [News API](https://newsapi.org)
---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('News App'),
+    );
+  }
+}

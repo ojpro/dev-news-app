905bfd8f2b4dc159012804c0a0fef7fc3c3f3cc1
ojpro <contact@ojpro.me>
2024-01-18 01:28:42 +0100

    feat(news): basic UI

--- lib/src/news/presentation/pages/news_page.dart
+++ lib/src/news/presentation/pages/news_page.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/common/components/box.dart';
+import 'package:news_app/core/common/components/text.dart';
 
-class NewsPage extends StatelessWidget{
+class NewsPage extends StatelessWidget {
   const NewsPage({super.key});
 
-  @override Widget build(BuildContext context){
-    return const Text('News Page');
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6,),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  boxComponent(
+                    child: textComponent(
+                      text: 'Technology',
+                      fontSize: 14.0,
+                      color: Colors.white,
+                    ),
+                    borderRadius: 24,
+                    backgroundColor: Colors.purple,
+                    padding: [4, 8, 4, 8],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

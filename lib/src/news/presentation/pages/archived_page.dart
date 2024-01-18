3c6de3fe56a5e72993f1c36167cbe0b6aa5b5246
ojpro <contact@ojpro.me>
2024-01-18 01:26:21 +0100

    feat(screens): added favorite and archived

---
+++ lib/src/news/presentation/pages/archived_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArchivedPage extends StatelessWidget {
+  const ArchivedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Archived');
+  }
+}

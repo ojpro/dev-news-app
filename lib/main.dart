14068901e727621ab6a15ba8a1cac6eabd632991
ojpro <contact@ojpro.me>
2024-01-18 01:25:23 +0100

    feat(Bloc): added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/src/app.dart';
 
+import 'core/common/states/bloc_observer.dart';
+
 void main(){
+  Bloc.observer = AppBlocObserver();
   runApp(const NewsApp());
 }
